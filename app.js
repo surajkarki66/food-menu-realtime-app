6a466e96b8b628635ecb8ff48e23cd8da0c08aee
surajkarki66 <suraj.karki500@protonmail.com>
2021-06-18 11:24:21 +0545

    Created websocket connection

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const http = require("http");
+const socketIO = require("socket.io");
 
 const db = require("./db");
 const config = require("./config");
 const app = express();
 
 const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected" + socket.id);
+});
 
 const port = config.PORT || 5003;
 
