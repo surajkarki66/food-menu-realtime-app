f7f77bf1fb544c8084bdb52aa21e03585b25951b
surajkarki66 <suraj.karki500@protonmail.com>
2021-06-18 13:16:01 +0545

    Implemented getData handler

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ io.on("connection", (socket) => {
   console.log("New client connected " + socket.id);
 
   // Returning the initial data of food menu from FoodItems collection
-  socket.on("initial_data", () => {
+  socket.on("initialData", () => {
     foodItems.find({}).then((docs) => {
       io.sockets.emit("getData", docs);
     });
--- client/src/components/PlaceOrder/PlaceOrder.js
+++ client/src/components/PlaceOrder/PlaceOrder.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Table, Container } from "reactstrap";
 import { useSocket } from "../../contexts/SocketProvider";
 
 const PlaceOrder = () => {
   const socket = useSocket();
+  const [foodData, setFoodData] = useState([]);
+
+  useEffect(() => {
+    if (socket == null) return;
+    // Loading initial menu
+    socket.emit("initialData");
+    socket.on("getData", getData);
+    return () => {
+      // clean up code
+      socket.off("getData", getData);
+    };
+  }, [socket]);
+  const getData = (foodItems) => {
+    foodItems = foodItems.map((food) => {
+      food.order = 0;
+      return food;
+    });
+    setFoodData(foodItems);
+  };
+  const changeQuantity = (event, foodId) => {
+    if (parseInt(event.target.value) < 0) {
+      event.target.value = 0;
+    }
+    const newArray = foodData.map((food) => {
+      if (food._id === foodId) {
+        food.order = parseInt(event.target.value);
+      }
+      return food;
+    });
+    setFoodData(newArray);
+  };
+
+  const getFoodData = () => {
+    return foodData.map((food) => {
+      return (
+        <tr key={food._id}>
+          <td> {food.name} </td>
+          <td>
+            <input
+              onChange={(e) => changeQuantity(e, food._id)}
+              value={food.order}
+              type="number"
+              placeholder="Quantity"
+            />
+          </td>
+          <td>
+            <button>Order</button>
+          </td>
+        </tr>
+      );
+    });
+  };
   return (
     <Container>
       <h2 className="h2Class">Order Menu</h2>
@@ -15,7 +67,7 @@ const PlaceOrder = () => {
             <th>Order</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{getFoodData()}</tbody>
       </Table>
     </Container>
   );
